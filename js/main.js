3cd3be6bfb760d1c1a6a7fc66adb5ba21b26edf8
Juan Espinet <juan.espinet@gmail.com>
2015-09-22 22:48:07 -0700

    hidden description and animations complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  
+  $('.card-description-button').on('click',function(event){
+    event.preventDefault();
+    var backcard = $(this).parents('.card-holder');
+    backcard.toggleClass('open');
+  });
 });
